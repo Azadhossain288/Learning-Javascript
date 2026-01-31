6952e180359e0370a6acbcd3d77539556795ba62
Azadhossain288 <azadhossain016288@gmail.com>
2026-01-31 20:17:59 +0600

    Modified array

--- arrays.js
+++ arrays.js
@@ -1,3 +1,5 @@
+/*
+
 const cars=["Saab","Volvo","BMW"];
 console.log(cars);
 
@@ -27,3 +29,45 @@ console.log(fruits.toString());
 ///using objects
 
 const person = {firstName:"John", lastName:"Doe", age:46};
+
+
+*/
+
+
+let name1,name2,name3,name4,name5;
+name1='Azad';
+name2='Asif';
+name3='Monjurul';
+name4='Abdur Rahman';
+
+document.write(name1+ '<br>');
+document.write(name2 + '<br>');
+document.write(name3 + '<br>');
+document.write(name4 + '<br>');
+
+let names=['Azad','Asif','Monjurul','Abdur Rahman'];
+
+document.write(names + '<br>');
+
+// Using push
+
+names.push('Khaled');
+document.write(names+ '<br>');
+
+// Using pop
+names.pop('Khaled');
+document.write(names + '<br>');
+
+document.write(names.length +'<br>');
+
+let cnt1=['Bangladesh','Pakistan'];
+let cnt2=[' Srilanka','Afganistan'];
+
+let total=cnt1+cnt2;
+
+document.write(total + '<br>');
+
+let country=cnt1.concat(cnt2);
+document.write(country + '<br>');
+
+
